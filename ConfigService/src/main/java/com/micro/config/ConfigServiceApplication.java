5f4243e249020ffb6c7226e04afd03947b950a5b
Paul <paul@kuruma.co.uk>
2019-01-14 15:20:18 +0000

    Add files via upload

---
+++ ConfigService/src/main/java/com/micro/config/ConfigServiceApplication.java
@@ -0,0 +1,17 @@
+package com.micro.config;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServiceApplication
+{
+	public static void main(final String[] args)
+	{
+		SpringApplication.run(ConfigServiceApplication.class, args);
+	}
+}
